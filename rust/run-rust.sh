ed5e034ac43b3b6ac28e7ea612f3b0ff8eb6fc7c
Philipp Hatt <philipp.hatt@gmail.com>
2020-08-09 14:35:21 +0200

    First version of rust environment

---
+++ rust/run-rust.sh
@@ -0,0 +1,16 @@
+# NOTE: run in this directory.
+
+# Build an image named 'rust-dev'
+docker build . -t rust-dev
+
+# Runs the ssh server at port 22222, same hostname as the host of the container.
+# Mounts the $HOME/src directory into the container.
+
+if [ ! "$(docker ps -q -f name=rust-dev)" ]; then
+    if [ "$(docker ps -aq -f status=exited -f name=rust-dev)" ]; then
+        # Need to remove the old 'rust-dev' container before running a new one.
+        docker rm rust-dev
+    fi
+    # run your container
+    docker run -p 22222:22 -it --mount="type=bind,source=$HOME/src,destination=/home/dev/src" --name=rust-dev -d rust-dev 
+fi
