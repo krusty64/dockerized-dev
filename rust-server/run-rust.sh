5ac2847b07fd729837da3ab290e5f1d50307ed12
krusty64 <philipp.hatt@gmail.com>
2020-08-23 20:20:30 +0200

    Fix bash script (don't use fish syntax).

--- rust-server/run-rust.sh
+++ rust-server/run-rust.sh
@@ -1,7 +1,7 @@
 # NOTE: run in this directory.
 
 # Build an image named 'rust-srv'
-docker build -t rust-srv --build-arg "user=$USER" --build-arg uid=(id -u) .
+docker build -t rust-srv --build-arg "user=$USER" --build-arg uid=`id -u` .
 
 # Runs the ssh server at port 22222, same hostname as the host of the container.
 # Mounts the $HOME/src directory into the container.
@@ -12,7 +12,10 @@ if [ ! "$(docker ps -q -f name=rust-srv)" ]; then
         docker rm rust-srv
     fi
     # run your container
-    docker run -p 127.0.0.1:8642:127.0.0.1:8080 -it \
+
+		# NOTE: The IP is only accessible from this computer, use an SSH forward:
+		# $ ssh -L 127.0.0.1:8642:8642 ...
+    docker run -p 127.0.0.1:8642:8080 -it \
 			--mount="type=bind,source=$HOME/src,destination=$HOME/src" \
 	    --mount="type=bind,source=$HOME/.gitconfig,destination=$HOME/.gitconfig,readonly" \
 			rust-srv
